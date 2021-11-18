da08a581d24ff57dfa83277536c4020b4c663c17
Amin0092 <aminnmadiev@mail.ru>
2021-11-18 22:39:01 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let four = document.querySelector('.four')
+let five = document.querySelector('.five')
+let six = document.querySelector('.six')
+let seven = document.querySelector('.seven')
+let eight = document.querySelector('.eight')
+let nine = document.querySelector('.nine')
+let ten = document.querySelector('.ten')
+let eleven = document.querySelector('.eleven')
+
+
+let q1 = document.querySelector(' .inter')
+let q2 = document.querySelector(' .age')
+let q3 = document.querySelector(' .documents')
+let q4 = document.querySelector(' .choice')
+let q5 = document.querySelector(' .place')
+let q6 = document.querySelector(' .prepare')
+let q7 = document.querySelector(' .comp')
+let q8 = document.querySelector(' .process')
+let q9 = document.querySelector(' .test')
+let q10 = document.querySelector('.certificate')
+let q11 = document.querySelector('.job')
+
+
+one.onclick = () => {
+    q1.classList.toggle('text2')
+}
+two.onclick = () => {
+    q2.classList.toggle('text2')
+}
+three.onclick = () => {
+    q3.classList.toggle('text2')
+}
+four.onclick = () => {
+    q4.classList.toggle('text2')
+}
+five.onclick = () => {
+    q5.classList.toggle('text2')
+}
+six.onclick = () => {
+    q6.classList.toggle('text2')
+}
+seven.onclick = () => {
+    q7.classList.toggle('text2')
+}
+eight.onclick = () => {
+    q8.classList.toggle('text2')
+}
+nine.onclick = () => {
+    q9.classList.toggle('text2')
+}
+ten.onclick = () => {
+    q10.classList.toggle('text2')
+}
+eleven.onclick = () => {
+    q11.classList.toggle('text2')
+}
